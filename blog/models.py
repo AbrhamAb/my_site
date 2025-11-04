f58b0331fec690a0d8a53f7bf90b7a1414be355d
Abrham Abebaw <abrhamabebaw.tech@gmail.com>
2025-11-04 11:12:26 +0300

    implement file upload successfully

--- blog/models.py
+++ blog/models.py
@@ -21,7 +21,7 @@ class Post(models.Model):
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True,db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
-    image_name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='posts', null=True, blank=True)
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
     tags = models.ManyToManyField(Tag)
 
